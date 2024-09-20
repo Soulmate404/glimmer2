7f059a30e2acb9f53a5c7c800734192ac3365b30
郭 <1440342414@qq.com>
2024-09-20 21:52:22 +0800

    对02的回答

--- scr/com/ISEKAI/HelloWorld.java
+++ scr/com/ISEKAI/HelloWorld.java
@@ -1,9 +1,15 @@
 package com.ISEKAI;
 /*声明程序所在的包*/
 import com.ISEKAI.tool.Print;
+
+
 /*导入Print类，提供print方法*/
 public class HelloWorld {
-    public static void main(String[] args){
+
+    public static void main(String [] args){
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
         Test.test();
     }
 }
@@ -14,3 +20,7 @@ class Test{
     }
 }
 /*创建test类，其中有test方法调用print类中的print方法打印目标字符串*/
+/*什么是包？根据我的理解
+* 包的地位类似于特殊文件夹，将不同功能的代码放置在不同的包中确保哥哥代码段不会互相冲突
+* 使代码更加模块化，便于操作
+* */
